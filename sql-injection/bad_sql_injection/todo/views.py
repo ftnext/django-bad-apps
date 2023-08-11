16f975e01c79a35294eccd739434fd2a9544ce1b
ftnext <takuyafjp+develop@gmail.com>
2023-08-11 13:56:46 +0900

    [feat] Show todo list

--- sql-injection/bad_sql_injection/todo/views.py
+++ sql-injection/bad_sql_injection/todo/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from .models import Todo
+
 
 def todo_list(request):
-    return render(request, "todo/list.html", {"todos": []})
+    todos = Todo.objects.all()
+    return render(request, "todo/list.html", {"todos": todos})
