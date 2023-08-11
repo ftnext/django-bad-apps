09882b8d02f974138628743e75653ce528a150ac
ftnext <takuyafjp+develop@gmail.com>
2023-08-11 14:13:31 +0900

    [feat] Filter todo (But bad implementation)

--- sql-injection/bad_sql_injection/todo/views.py
+++ sql-injection/bad_sql_injection/todo/views.py
@@ -4,5 +4,13 @@ from .models import Todo
 
 
 def todo_list(request):
-    todos = Todo.objects.all()
+    if "todo" not in request.GET:
+        todos = Todo.objects.all()
+    else:
+        todo_str = request.GET["todo"]
+        sql = (
+            "SELECT id, id_str, todo, created_date, due_date FROM todos "
+            "WHERE todo = '{}';".format(todo_str)
+        )
+        todos = Todo.objects.raw(sql)
     return render(request, "todo/list.html", {"todos": todos})
