82fe5fb7254489ff9c1f198e336027ca8ad4685c
ftnext <takuyafjp+develop@gmail.com>
2023-08-11 13:49:59 +0900

    [feat] todolist page (0 todos)

--- sql-injection/bad_sql_injection/todo/models.py
+++ sql-injection/bad_sql_injection/todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Todo(models.Model):
+    id_str = models.CharField("ID", max_length=20)
+    todo = models.CharField("todo", max_length=100)
+    created_date = models.DateTimeField("登録日", default=timezone.now)
+    due_date = models.DateField("期限", blank=False, null=False)
+
+    class Meta:
+        db_table = "todos"
