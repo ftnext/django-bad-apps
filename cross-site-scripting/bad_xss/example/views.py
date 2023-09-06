22bf2f01d13aa66d15acf84234b6fd43e7a07510
ftnext <takuyafjp+develop@gmail.com>
2023-09-06 21:10:26 +0900

    [feat] Templates escape injected scripts

--- cross-site-scripting/bad_xss/example/views.py
+++ cross-site-scripting/bad_xss/example/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
+from django.template.response import TemplateResponse
 
 
 def example(request):
     # return HttpResponse("Hello world")
-    return HttpResponse('<script>alert("XSSです")</script>')
+    # return HttpResponse('<script>alert("XSSです")</script>')
+
+    context = {"message": '<script>alert("XSSです")</script>'}
+    return TemplateResponse(request, "example/index.html", context)
