bbfcdc408d51484b77d5cc691b5bc55bec81fc5e
ftnext <takuyafjp+develop@gmail.com>
2023-09-06 21:08:11 +0900

    [feat] XSS with HttpResponse 🙅‍♂️

--- cross-site-scripting/bad_xss/example/views.py
+++ cross-site-scripting/bad_xss/example/views.py
@@ -2,4 +2,5 @@ from django.http import HttpResponse
 
 
 def example(request):
-    return HttpResponse("Hello world")
+    # return HttpResponse("Hello world")
+    return HttpResponse('<script>alert("XSSです")</script>')
