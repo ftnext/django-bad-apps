2c0c457c1adb6d8eaa02f45e8738da7488d321b7
ftnext <takuyafjp+develop@gmail.com>
2023-09-06 21:13:35 +0900

    [docs] Escape!!

--- cross-site-scripting/bad_xss/example/views.py
+++ cross-site-scripting/bad_xss/example/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
 from django.template.response import TemplateResponse
+from django.utils.html import escape
 
 
 def example(request):
     # return HttpResponse("Hello world")
-    # return HttpResponse('<script>alert("XSSです")</script>')
+    return HttpResponse('<script>alert("XSSです")</script>')
+    # Correct implementation:
+    # return HttpResponse(escape('<script>alert("XSSです")</script>'))
 
-    context = {"message": '<script>alert("XSSです")</script>'}
-    return TemplateResponse(request, "example/index.html", context)
+    # context = {"message": '<script>alert("XSSです")</script>'}
+    # return TemplateResponse(request, "example/index.html", context)
