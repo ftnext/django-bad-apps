78441c4da83df1e96fe12495e2f4109702eefdab
ftnext <takuyafjp+develop@gmail.com>
2023-10-03 22:41:02 +0900

    [chore] SQL injection Django app image

--- sql-injection/bad_sql_injection/config/settings.py
+++ sql-injection/bad_sql_injection/config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0"]
 
 
 # Application definition
